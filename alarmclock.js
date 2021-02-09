bbaed6f77c0a9e0806d4c2714a4e049054dd8816
Bozena611 <bozena.vuckovic@gmail.com>
2021-02-09 15:48:53 +0100

    set_stop working

--- alarmclock.js
+++ alarmclock.js
@@ -1,12 +1,31 @@
-function setAlarm() {}
+function setAlarm() {
+	const timeRemaining = document.getElementById("timeRemaining");
+  let setAlarm = document.getElementById("alarmSet");
+  let time = setAlarm.valueAsNumber;
+    
+  if (time < 10) {
+  	timeRemaining.innerText = "Time Remaining: 00:" + time;
+  } else {
+    timeRemaining.innerText = "Time Remaining: 00:0 " + time;
+  }
 
+  let counter = time;
 
+  function countdown(counter) {
+    if (counter > 0) {
+      counter--;
+      setTimeout(function() {countdown(counter)}, 1000);
+      timeRemaining.innerText = "Time Remaining: 00:" + (counter < 10 ? "0" + counter : counter);
+    } else {
+    	timeRemaining.style.backgroundColor = "red";
+      playAlarm()
+    }
+  }
 
+  countdown(counter);
+}
 
-
-// DO NOT EDIT BELOW HERE
-
-var audio = new Audio("alarmsound.mp3");
+let audio = new Audio("alarmsound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
